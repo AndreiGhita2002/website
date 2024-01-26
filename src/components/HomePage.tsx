4bb115b24f102c3addbca0f4351e98c8c81d8a98
andrei <andrei.ghita2002@gmail.com>
2024-01-26 10:22:10 +0000

    basic website

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,78 @@
+import '../styles/index.css'
+import React from "react";
+
+export function HomePage() {
+    return (
+        <div className='HomePage'>
+            <CardColumn className='MainColumn'>
+                <TextCard>
+                    <h1 className='TitleText'>
+                        ANDREI IULIU GHITA
+                    </h1>
+                </TextCard>
+                <TextCard>
+                    <p className='MainText'>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, aspernatur blanditiis distinctio
+                        dolores eligendi est fuga illum ipsam modi nulla officiis praesentium quidem repellendus vel voluptas
+                        voluptatem voluptates voluptatum! Exercitationem?
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, aspernatur blanditiis distinctio
+                        dolores eligendi est fuga illum ipsam modi nulla officiis praesentium quidem repellendus vel voluptas
+                        voluptatem voluptates voluptatum! Exercitationem?
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, aspernatur blanditiis distinctio
+                        dolores eligendi est fuga illum ipsam modi nulla officiis praesentium quidem repellendus vel voluptas
+                        voluptatem voluptates voluptatum! Exercitationem?
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, aspernatur blanditiis distinctio
+                        dolores eligendi est fuga illum ipsam modi nulla officiis praesentium quidem repellendus vel voluptas
+                        voluptatem voluptates voluptatum! Exercitationem?
+                    </p>
+                </TextCard>
+            </CardColumn>
+            <CardColumn className='SideColumn'>
+                <TextCard>
+                    <p className='ContactText'>
+                        CONTACT INFO: <br/>
+                        email <br/>
+                        discord <br/>
+                        other <br/>
+                    </p>
+                </TextCard>
+                <TextCard>
+                    <p className='PalsText'>
+                        pals
+                    </p>
+                </TextCard>
+            </CardColumn>
+        </div>
+    )
+}
+
+
+interface CardColumnProps {
+    children?: React.ReactNode,
+    className?: string,
+}
+
+export function CardColumn({children, className}: CardColumnProps) {
+    let cn = 'CardColumn'
+    if (className) {
+        cn += ' ' + className
+    }
+    return(
+        <div className={cn}>
+            {children}
+        </div>
+    )
+}
+
+
+interface TextCardProps {
+    children?: React.ReactNode
+}
+
+export function TextCard({children}: TextCardProps) {
+    return (
+        <div className='TextCard'>
+            {children}
+        </div>
+    )
+}
