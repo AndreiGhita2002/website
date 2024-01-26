6c94c193bf859cefee7a68ed111ef35afe1396bc
andrei <andrei.ghita2002@gmail.com>
2024-01-26 13:39:10 +0000

    made the planet rotate
    Signed-off-by: andrei <andrei.ghita2002@gmail.com>

--- src/components/Planets.tsx
+++ src/components/Planets.tsx
@@ -1,31 +1,40 @@
-import React from 'react';
-import { Canvas } from '@react-three/fiber';
+import React, {useRef} from 'react';
+import {Canvas, useFrame, useLoader} from '@react-three/fiber';
+import {Texture, TextureLoader} from "three";
 
 
 interface PlanetProps {
     position: [number, number, number],
     sphere_args?: [number, number, number],
+    colorMap?: Texture
 }
 
-function Planet({position, sphere_args}: PlanetProps) {
+function Planet({position, sphere_args, colorMap}: PlanetProps) {
+    const meshRef: any = useRef();
+    const rotSpeed = 0.1;
+    useFrame((state, delta) => {
+        // @ts-ignore
+        meshRef.current.rotation.y += delta * rotSpeed;
+    })
     if (!sphere_args) {
-        sphere_args = [1, 16, 16]
+        sphere_args = [2, 32, 32]
     }
     return (
-        <mesh position={position}>
+        <mesh ref={meshRef} position={position}>
             <sphereGeometry args={sphere_args}/>
-            <meshStandardMaterial color={'orange'} />
+            <meshStandardMaterial color={'yellow'} map={colorMap}/>
         </mesh>
     );
 }
 
 export function BackgroundCanvas() {
+    const colorMap = useLoader(TextureLoader, 'PavingStones092_1K-PNG_Color.png');
     return (
         <div className="CanvasContainer">
             <Canvas>
-                <Planet position={[0, 0, 0]}/>
-                <ambientLight intensity={0.1}/>
-                <directionalLight color="red" position={[0, 0, 5]}/>
+                <Planet position={[0, 0, 0]} colorMap={colorMap}/>
+                <ambientLight intensity={1.0}/>
+                <directionalLight color="red" position={[0, 5, 0]}/>
             </Canvas>
         </div>
     );
